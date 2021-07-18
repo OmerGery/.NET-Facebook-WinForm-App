d75074f84e3bbf30cd8926934962dbdba67d6c56
OmerGery <omergery1996@gmail.com>
2021-07-18 12:51:27 +0300

    start

--- FacebookWinFormsApp/FormMain.cs
+++ FacebookWinFormsApp/FormMain.cs
@@ -13,26 +13,41 @@ namespace BasicFacebookFeatures
 {
     public partial class FormMain : Form
     {
+        private bool m_IsLoggedIn;
+        private LoginResult m_loginResult;
         public FormMain()
         {
+            
+            m_IsLoggedIn = false;
             InitializeComponent();
             FacebookWrapper.FacebookService.s_CollectionLimit = 100;
         }
 
         private void buttonLogin_Click(object sender, EventArgs e)
         {
-            Clipboard.SetText("design.patterns20cc"); /// the current password for Desig Patter
+            if(!m_IsLoggedIn)
+            {
+                Clipboard.SetText("design.patterns20cc"); /// the current password for Desig Patter
 
-            FacebookWrapper.LoginResult loginResult = FacebookService.Login(
+                m_loginResult = FacebookService.Login(
                     /// (This is Desig Patter's App ID. replace it with your own)
-                    "1450160541956417", 
+                    "1450160541956417",
                     /// requested permissions:
-					"email",
+                    "email",
                     "public_profile"
                     /// add any relevant permissions
-                    );
-
-            buttonLogin.Text = $"Logged in as {loginResult.LoggedInUser.Name}";
+                    
+                );
+                if(m_loginResult.FacebookOAuthResult.IsSuccess)
+                {
+                    m_IsLoggedIn = true;
+                    buttonLogin.Text = $"Logged in as {m_loginResult.LoggedInUser.Name}";
+                }
+            }
+            else
+            {
+                buttonLogin.Text = m_loginResult.LoggedInUser.Birthday;
+            }
         }
 
         private void buttonLogout_Click(object sender, EventArgs e)
