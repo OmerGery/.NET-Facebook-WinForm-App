6b3b3b2cdf84b3aff18bc6235e573dd1ad0883a9
OmerGery <omergery1996@gmail.com>
2021-07-27 21:16:59 +0300

    tel aviv festival

---
+++ FacebookWinFormsApp/FormFlowManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFacebookFeatures
+{
+    public class FormFlowManager
+    {
+        public readonly StartForm m_StartForm = new StartForm();
+        public FormMain m_FacebookForm;
+
+        public void RunApp()
+        {
+            m_StartForm.ShowDialog();
+            m_FacebookForm = new FormMain(m_StartForm.m_LoggedUser);
+            m_FacebookForm.ShowDialog();
+        }
+    }
+}
