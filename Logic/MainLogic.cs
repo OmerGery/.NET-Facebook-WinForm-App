59c61fd6f4bcdf9ceaab54a8c0680f6320743de6
OmerGery <omergery1996@gmail.com>
2021-08-04 20:26:02 +0300

    started separting main form

---
+++ Logic/MainLogic.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using FacebookWrapper;
+using FacebookWrapper.ObjectModel;
+
+namespace Logic
+{
+    public class MainLogic
+    {
+        public User LoggedUser { get; };
+
+        public string AccessToken { get; }
+
+        public MainLogic(LoginResult i_LoginResult)
+        {
+            LoggedUser = i_LoginResult.LoggedInUser;
+            AccessToken = i_LoginResult.AccessToken;
+        }
+
+        public string GetUserName()
+        {
+            return LoggedUser.UserName;
+        }
+
+        public Image GetProfilePicture()
+        {
+            return LoggedUser.ImageSmall;
+        }
+    }
+}
