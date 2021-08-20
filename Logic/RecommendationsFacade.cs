16aa5c694c8f681acf9399cacbc45b94ec060404
dolev7 <d.dolev7@gmail.com>
2021-08-20 19:04:36 +0300

    facade wrongly little working

---
+++ Logic/RecommendationsFacade.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using FacebookWrapper.ObjectModel;
+
+namespace Logic
+{
+    public class RecommendationsFacade
+    {
+        private LastFmApi m_LastFmApi = new LastFmApi();
+
+        async public Task<Dictionary<string, List<string>>> GetArtistRecommendations(
+            IFacebookUser i_LoggedUser,
+            AppSettings i_AppSettings,
+            int i_SimilarArtistsAmount)
+        {
+            Dictionary<string, List<string>> artistsDictionary = new Dictionary<string, List<string>>();
+            List<string> userFavoriteArtists = new List<string>();
+            if(i_AppSettings.IsMockState)
+            {
+                userFavoriteArtists = MocksGenerator.GetFakeArtists();
+            }
+            else
+            {
+                foreach(Page artistPage in i_LoggedUser.GetLikedPages())
+                {
+                    if(artistPage.Category == "Artist")
+                    {
+                        userFavoriteArtists.Add(artistPage.Name);
+                    }
+                }
+            }
+
+            foreach(string favoriteArtist in userFavoriteArtists)
+            {
+                XDocument userSimilarArtists =
+                    await m_LastFmApi.GetSimilarArtists(favoriteArtist, i_SimilarArtistsAmount);
+                List<string> userSimilarArtistsList = m_LastFmApi.FilterSimilarArtists(userSimilarArtists);
+                artistsDictionary.Add(favoriteArtist, userSimilarArtistsList);
+            }
+
+            return artistsDictionary;
+        }
+    }
+}
