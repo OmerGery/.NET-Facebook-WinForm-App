ccd61f07da1dfd33207b0cfda4408ad7fd59edb1
OmerGery <omergery1996@gmail.com>
2021-09-17 15:10:31 +0300

    added templeta method

---
+++ Logic/PhotoSelectionAlgorithmBase.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper.ObjectModel;
+
+namespace Logic
+{
+    public abstract class PhotoSelectionAlgorithmBase
+    {
+        public Image GetPhoto(User i_FacebookUser)
+        {
+
+            FacebookObjectCollection<Photo> taggedPictures = i_FacebookUser.PhotosTaggedIn;
+            if (taggedPictures.Count < 1)
+            {
+                throw new Exception("No Tagged pictures");
+            }
+
+            int photoIndex = GetIndexOfPhoto(taggedPictures);
+            return taggedPictures[photoIndex].ImageAlbum;
+        }
+
+        public abstract int GetIndexOfPhoto(FacebookObjectCollection<Photo> i_Album);
+    }
+
+    public class PhotoRandomizer : PhotoSelectionAlgorithmBase
+    {
+
+        private readonly Random r_Random = new Random();
+
+        public override int GetIndexOfPhoto(FacebookObjectCollection<Photo> i_Album)
+        {
+            return r_Random.Next(i_Album.Count);
+        }
+    }
+    
+
+}
