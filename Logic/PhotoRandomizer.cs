27d807f3b5a010a9598232dc059f96acbce98c80
OmerGery <omergery1996@gmail.com>
2021-09-18 20:49:25 +0300

    .

---
+++ Logic/PhotoRandomizer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FacebookWrapper.ObjectModel;
+
+namespace Logic
+{
+    public class PhotoRandomizer : PhotoSelectionAlgorithmBase
+    {
+
+        private readonly Random r_Random = new Random();
+
+        public override Image GetImageFromAlbum(FacebookObjectCollection<Photo> i_Album)
+        {
+            return i_Album[r_Random.Next(i_Album.Count)].ImageAlbum;
+        }
+    }
+}
