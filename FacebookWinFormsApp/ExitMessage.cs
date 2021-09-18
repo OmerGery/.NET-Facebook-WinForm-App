2f9644f8edf2d576c8b78aeef8774660296e1d79
OmerGery <omergery1996@gmail.com>
2021-09-18 20:14:51 +0300

    finished

---
+++ FacebookWinFormsApp/ExitMessage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Logic;
+namespace BasicFacebookFeatures
+{
+    public class ExitMessage : IExitObserver
+    {
+        public void UpdateExited()
+        {
+            MessageBox.Show("Bye Bye :)");
+        }
+    }
+}
